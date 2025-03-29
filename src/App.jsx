1e2927a77a9b4a33e2cde222429f92cbc7e5c9b8
brendassales08 <brendassales08@gmail.com>
2025-03-29 20:31:09 +0000

    fim da criiação e estilização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import iconStar from "./assets/icon-star.svg"
+import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 
 export function App (){
@@ -47,7 +48,12 @@ export function App (){
 
     ) : (
       <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
-        <p>{notaAvaliacao}</p>
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="imagem de agradecimento" />
+
+        <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6 ">You selected {notaAvaliacao} out of 5</p>
+
+        <h1 className="text-2xl font-bold mb-2.5 text-center">Thank You</h1>
+        <p className="text-sm text-light-grey leadind-1 text-center ">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
       </div>
     ) 
   )
