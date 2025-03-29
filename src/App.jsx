34a77077154031568317f30e8327bb7e21b593ff
brendassales08 <brendassales08@gmail.com>
2025-03-29 19:51:37 +0000

    adaptação da troca de card ao escolher uma nota de avaliacao

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ export function App (){
   }
 
   return(
-    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+    notaAvaliacao === 0 ? (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
       <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
         <img src={iconStar} alt="ícone de estrela" />
       </div>
@@ -26,6 +27,12 @@ export function App (){
       <button className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
       
 
-    </div>
+    </div> 
+
+    ) : (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+        <p>{notaAvaliacao}</p>
+      </div>
+    ) 
   )
 }
